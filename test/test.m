fbcff70fc0e4fbccb5ce9c60f822d40afe7597e2
Eve <Eve@Eves-MacBook-Pro.local>
2021-12-03 11:52:21 -0500

    Completed basic metrics

--- test/test.m
+++ test/test.m
@@ -6,7 +6,20 @@ filename = '/Users/Eve/Eve/2233/MBPAR/Data/blktrace.txt';
 blktrace_parser(filename);
 
 %% 3. Calculate the basic metrics
-% The following is based on batch_analysis
+% The following is based on batch_analysis in MBPAR
 if size(lists_action,2)==7
-    lists_action(:,1:2)=lists_action(:,6:7);
+    lists_action(:,1:2)=lists_action(:,6:7); % copied over col6 and col7 to col1 and col2
 end
+
+% options are not used, but created for the purpose of signature matching
+options.plot_fontsize=10;
+
+% sort lists_action by the arrival times
+[lists_action,idx]=sortrows(lists_action,1);
+% sort lists_cmd accordingly
+lists_cmd=lists_cmd(idx,:);
+
+basic_info=sub_basic_info(lists_action,lists_cmd,options);
+
+%% 4. Calculate the advanced metrics
+
